eb29d9432d8d35a994ff4768ca60d815f7cd7faf
Charlie Roberts <charlie@robertsclan.co.uk>
2024-01-22 12:49:37 +0000

    cr-themetoggle-scriptilitious.js

---
+++ scriptilicious.js
@@ -0,0 +1,14 @@
+function toggleTheme(value) { 
+  
+  
+    // Get the link element for the stylesheet
+    var linkElement = document.getElementById('theme-stylesheet');
+    
+    // Obtain the name of stylesheet as a parameter and set it using href attribute.
+    linkElement.href = value; 
+    
+    var sheets = document 
+        .getElementsByTagName('link'); 
+
+    sheets[0].href = value; 
+}
